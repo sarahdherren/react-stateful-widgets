17c72928cda156ff62d247fda5f6ed2d22fa766e
crash138 <67919714+sarah-herren@users.noreply.github.com>
2021-01-27 18:28:14 -0600

    moods & spinner complete

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -37,23 +37,26 @@ STEP 4:
   Do you remember the operator we use to do "not"?
 */
 
-import React from 'react'; /* STEP 0 */
+import React, { useState } from 'react';
 
 export default function Spinner() {
-/* STEP 1 */
+  let [spinner, setSpinner] = useState(true);
 
   const toggleSpinner = () => {
-  /* STEP 4 */
+    const hide = document.querySelector('#spinner');
+    spinner === true ? setSpinner(spinner = false) : setSpinner(spinner = true)
+    
+    spinner === true ?  hide.style.display = '' : hide.style.display = 'none';
   };
 
   return (
     <div className='widget-spinner container'>
       <h2>Spinner</h2>
       {
-        true && <div id='spinner' className='spinner'>--+--</div> /* STEP 2 */
+        {spinner} && <div id='spinner' className='spinner'>--+--</div> /* STEP 2 */
       }
       <button id='toggleSpinner' onClick={toggleSpinner}>
-        Hide Spinner {/* STEP 3 */}
+        {spinner != true ? 'show spinner' : 'hide spinner'}
       </button>
     </div>
   );
